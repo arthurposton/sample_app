811d00496d5a7ef2a6312a7aa5a27329c5385cce
arthurposton <arthur.poston@focuseddevelopment.net>
2013-04-23 13:10:33 -0400

    User model with passwords, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+# By using the symbol ':user', we get Factory Girl to simulate the User model.
+Factory.define :user do |user|
+  user.name			"Arthur George Poston Jr."
+  user.email			"arthur@focuseddevelopment.net"
+  user.password			"basketball"
+  user.password_confirmation	"basketball"
+end
